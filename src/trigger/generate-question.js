ecc34720c034e6919d89e2bc977edc1014abd36b
Nalendra Praja <nalendrapm@unitedtractors.com>
2025-07-20 17:47:41 +0700

    minor bug

--- src/trigger/generate-question.js
+++ src/trigger/generate-question.js
@@ -43,7 +43,7 @@ export async function GenerateQuestionAI(skill,assessmentId) {
 });
 
     try {
-        const response = await fetch("http://localhost:3001/api/assessment",{
+        const response = await fetch("https://job-pilot-five.vercel.app/api/assessment",{
             method: "POST",
             headers: {
                     "Content-Type": "application/json"
